f758b1bf574375fcaeb877f82dfb7c6cd898bda8
Constantin-Radu Mirescu <radu.mirescu@rinftech.com>
2017-03-18 10:33:41 +0200

    ecuatie imposibila + ec grad 1

--- sandbox/js/ec_grad2.js
+++ sandbox/js/ec_grad2.js
@@ -3,7 +3,11 @@ function ec_grad2(a,b,c) {
     if( b == 0 ) {
       if( c == 0 ) {
         return "ecuatie nedeterminata!";
+      } else {
+        return "ecuatie imposibila!";
       }
+    } else {
+      return -c/b;
     }
   }
 }
--- sandbox/js/tests.js
+++ sandbox/js/tests.js
@@ -3,5 +3,13 @@ QUnit.test( "hello test", function( assert ) {
 });
 
 QUnit.test("ecuatie nedeterminata", function(assert){
-  assert.ok( ec_grad2(0,0,0) == "ecuatie nedeterminata!", "Passed!");
+  assert.ok( ec_grad2(0,0,0) == "ecuatie nedeterminata!", "Caz ecuatie nedeterminata a esuat");
+});
+
+QUnit.test("ecuatie imposibila", function(assert){
+  assert.ok( ec_grad2(0,0,12) == "ecuatie imposibila!", "Test de ecuatie imposibila esuat");
+});
+
+QUnit.test("ecuatie grad 1", function(assert){
+  assert.equal( ec_grad2(0,5,10), -2, "x =");
 });
