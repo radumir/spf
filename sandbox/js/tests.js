21893e50c6623fde3b41fd37e9035ca6810a3949
Constantin-Radu Mirescu <radu.mirescu@rinftech.com>
2017-03-18 10:03:47 +0200

    test ecuatie nedeterminata

--- sandbox/js/tests.js
+++ sandbox/js/tests.js
@@ -1,3 +1,7 @@
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test("ecuatie nedeterminata", function(assert){
+  assert.ok( ec_grad2(0,0,0) == "ecuatie nedeterminata!", "Passed!");
+});
