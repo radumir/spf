d8746f2ef3a34afe735d57409fb1d0b25d0b030a
Constantin-Radu Mirescu <radu.mirescu@rinftech.com>
2017-03-22 09:11:41 +0200

    Define the invokeDrawFunction

--- sandbox/js/mindmap.js
+++ sandbox/js/mindmap.js
@@ -7,6 +7,11 @@ function setup(){
   createCanvas(MindMapOptions.width, MindMapOptions.height);
 }
 
+function invokeDrawFunction( args ) {
+  var fName = args[0];
+  window[fName](args[1],args[2],args[3],args[4]);
+}
+
 function draw(){
-  ellipse(50, 50, 80, 80);
+  invokeDrawFunction(['ellipse',50,50,80,80]);
 }
