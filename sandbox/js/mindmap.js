c97a2659500bcbfae73db65035ff7c9947f7aec5
Constantin-Radu Mirescu <radu.mirescu@rinftech.com>
2017-03-22 08:58:14 +0200

    Am un firstSlide cu rootNode

---
+++ sandbox/js/mindmap.js
@@ -0,0 +1,12 @@
+var MindMapOptions = {
+  width:800,
+  height:600
+};
+
+function setup(){
+  createCanvas(MindMapOptions.width, MindMapOptions.height);
+}
+
+function draw(){
+  ellipse(50, 50, 80, 80);
+}
