7d50f732727950f2ef46025b9a72485854b904ab
Constantin-Radu Mirescu <radu.mirescu@rinftech.com>
2017-04-07 08:47:57 +0300

    schimbat structura astfel incat nodul sa fie identificat dupa cheie

--- sandbox/js/content.js
+++ sandbox/js/content.js
@@ -1,13 +1,12 @@
 var mindMap = {
-  nodes:[
-    {
+  nodes:{
+    n1:{
       text:'Programare',
-      content: [
-        { text:'Generalități' },
-        { text: 'Mașină'},
-        { text: 'Corespondență'},
-        { text: 'Javascript'},
-      ]
-    }      
-  ]
+      content: ['n2','n3','n4','n5']
+    },
+    n2:{ text:'Generalități' },
+    n3:{ text: 'Mașină'},
+    n4:{ text: 'Corespondență'},
+    n5:{ text: 'Javascript'}
+  }
 };
